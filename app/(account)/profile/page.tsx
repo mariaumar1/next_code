fa33aafa8e31c2163f3b5415495e2568869ba326
Maria Umar <101625594+mariaumar1@users.noreply.github.com>
2023-06-01 16:31:59 +0500

    Add files via upload

---
+++ app/(account)/profile/page.tsx
@@ -0,0 +1,83 @@
+import Link from "next/link";
+const profile = () => {
+  return (
+    <>
+      <h1>Popular Jobs Categories</h1>
+      <div class="grid grid-cols-4 gap-1">
+        <div class="col-3">
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>
+          <Link href="">Read More</Link>
+        </div>
+        <div class="col-3">
+          {" "}
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>{" "}
+          <Link href="">Read More</Link>
+        </div>
+        <div class="col-3">
+          {" "}
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>{" "}
+          <Link href="">Read More</Link>
+        </div>
+        <div class="col-3">
+          {" "}
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>{" "}
+          <Link href="">Read More</Link>
+        </div>
+        <div class="col-3">
+          {" "}
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>{" "}
+          <Link href="">Read More</Link>
+        </div>
+        <div class="col-3">
+          {" "}
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>{" "}
+          <Link href="">Read More</Link>
+        </div>
+        <div class="col-3">
+          {" "}
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>
+          <Link href="">Read More</Link>
+        </div>
+        <div class="col-3">
+          {" "}
+          <h6>job title</h6>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab,
+            dignissimos!
+          </p>
+          <Link href="">Read More</Link>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default profile;
